76698fee7397b4af7bf438ae4d07a4cac0793177
joao <joaodevcosta@gmail.com>
2023-08-06 17:36:41 -0300

    Reset css and creating components.

---
+++ portfolio-joao/src/app/last-projects/last-projects.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-last-projects',
+  templateUrl: './last-projects.component.html',
+  styleUrls: ['./last-projects.component.css']
+})
+export class LastProjectsComponent {
+
+}
